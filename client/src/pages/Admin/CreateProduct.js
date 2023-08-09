34268dc13cfa601e31269ec2c574cdf2618fc4d8
kunal786ks <ks445kunalsharma@gmail.com>
2023-08-09 17:34:30 +0530

    nav

--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -54,7 +54,7 @@ const CreateProduct = () => {
         toast.error(data?.message);
       } else {
         toast.success("Product Created Successfully");
-        navigate("/");
+        navigate("/dashboard/admin/products");
       }
     } catch (error) {
       console.log(error);
