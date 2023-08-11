2d1dd89388ea717de465998c53ecf11cf8251dac
kunal786ks <ks445kunalsharma@gmail.com>
2023-08-11 15:19:31 +0530

    last

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 
+
+
+//this is server
+
+
+
+
+
+
+
+
+
+
+
 //configure env
 dotenv.config();
 
